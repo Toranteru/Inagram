4ad913a90f6aa85884dfeedfc7764032de6214ad
Toranteru <toranteru@gmail.com>
2022-04-15 10:12:21 -0400

    Update client view with responsive captions.

--- client/src/components/Feed/Feed.js
+++ client/src/components/Feed/Feed.js
@@ -7,14 +7,16 @@ function feedView(posts) {
   let post = posts.map((post, key) => {
     return (
       <div key={key} className='flex post shadow'>
-        <div className='post-title-container'>
+        <div className='post-title-container shadow'>
           <div className='post-title'>{post.title}</div>
         </div>
         <div className='post-image-container flex center'>
          <img className='post-image' src={`http://localhost:8080/posts/image/${post.filename}`} alt='Inagram Post'></img>
         </div>
-        <div className='post-caption-container'>
-          <div className='post-caption'>{post.caption}</div>
+        <div className='post-caption-container center'>
+          <div style={{ padding: 16}}>
+            <p className='post-caption'>{post.caption}</p>
+          </div>
         </div>
       </div>
     )
